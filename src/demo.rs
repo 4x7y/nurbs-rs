4feb058cd60c8783c321e399e5993ad5556b533e
Xue Yuechuan <xueyuechuan@gmail.com>
2020-02-10 22:46:12 -0600

    b-spline surface

--- src/demo.rs
+++ src/demo.rs
@@ -3,8 +3,11 @@ extern crate nalgebra as na;
 use crobot::geometry::bezier;
 use kiss3d::light::Light;
 use kiss3d::window::Window;
-use na::{Point3, Vector3};
+use kiss3d::resource::Mesh;
+use na::{Point3, Vector3, UnitQuaternion};
 use std::{thread, time};
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn sleep(millis: u64) {
     let duration = time::Duration::from_millis(millis);
@@ -12,37 +15,91 @@ fn sleep(millis: u64) {
 }
 
 fn bezier_tests() {
+//    let control_points = vec![
+//        Vector3::new(-0.1, -0.1, 0.0),
+//        Vector3::new(0.0, 0.1, 0.0),
+//        Vector3::new(0.1, -0.1, 0.1),
+//        Vector3::new(-0.1, 0.0, 0.3),
+//    ];
+//    let weights = vec![1., 20., 1., 4.];
+//
+//
+//    let bezier_curve = bezier::BezierCurve::new(control_points.clone());
+//    let bc = bezier_curve.get_curve(100);
+//    let rational_bezier_curve = bezier::RationalBezierCurve::new(control_points, weights);
+//    let rbc = rational_bezier_curve.get_curve(100);
+
     let control_points = vec![
-        Vector3::new(-0.1, -0.1, 0.0),
-        Vector3::new(0.0, 0.1, 0.0),
-        Vector3::new(0.1, -0.1, 0.1),
-        Vector3::new(-0.1, 0.0, 0.3),
+        Vector3::new(0.0, 0.0,  0.0),
+        Vector3::new(0.0, 4.0,  0.0),
+        Vector3::new(0.0, 8.0, -3.0),
+        Vector3::new(2.0, 0.0,  6.0),
+        Vector3::new(2.0, 4.0,  0.0),
+        Vector3::new(2.0, 8.0,  0.0),
+        Vector3::new(4.0, 0.0,  0.0),
+        Vector3::new(4.0, 4.0,  0.0),
+        Vector3::new(4.0, 8.0,  3.0),
+        Vector3::new(6.0, 0.0,  0.0),
+        Vector3::new(6.0, 4.0, -3.0),
+        Vector3::new(6.0, 8.0,  0.0),
     ];
-    let weights = vec![1., 20., 1., 4.];
+    let knot_vector_u = vec![
+        0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0,
+    ];
+    let knot_vector_v = vec![
+        0.0, 0.0, 0.0, 1.0, 1.0, 1.0,
+    ];
 
+    let surface = bezier::BSplineSurface::new(
+        control_points,
+        knot_vector_u,
+        knot_vector_v,
+        3, // degree_u
+        2, // degree_v
+        4, // size_u
+        3, // size_v
+    );
 
-    let bezier_curve = bezier::BezierCurve::new(control_points.clone());
-    let bc = bezier_curve.get_curve(100);
-    let rational_bezier_curve = bezier::RationalBezierCurve::new(control_points, weights);
-    let rbc = rational_bezier_curve.get_curve(100);
+    let point = surface.evaluate_single(0.1, 0.2);
+    println!("{}", point);
+
+    let mut vertices = Vec::new();
+    let mut indices = Vec::new();
+    for i in 0u16..100 {
+        for j in 0u16..100 {
+            let u = i as f32 / 100.0;
+            let v = j as f32 / 100.0;
+            let coord = surface.evaluate_single(u, v);
+            vertices.push(Point3::from(coord));
+
+            if i > 0 && j > 0 {
+                let idx_1 = i * 100 + j;
+                let idx_2 = (i - 1) * 100 + j;
+                let idx_3 = i * 100 + (j - 1);
+                indices.push(Point3::new(idx_1, idx_2, idx_3))
+            }
+
+            if i < 99 && j < 99 {
+                let idx_1 = i * 100 + j;
+                let idx_2 = (i + 1) * 100 + j;
+                let idx_3 = i * 100 + (j + 1);
+                indices.push(Point3::new(idx_1, idx_2, idx_3))
+            }
+        }
+    }
+
+    let mesh = Rc::new(RefCell::new(Mesh::new(
+        vertices, indices, None, None, false,
+    )));
 
     let mut window = Window::new("[ME 625] Bezier Demo");
     window.set_light(Light::StickToCamera);
 
+    let mut c = window.add_mesh(mesh, Vector3::new(1.0, 1.0, 1.0));
+    c.set_color(1.0, 0.0, 0.0);
+    c.enable_backface_culling(false);
+
     while window.render() {
-        let red   = Point3::new(1.0, 0.0, 0.0);
-
-        for i in 1..=100 {
-            let s = Point3::from(bc[i - 1]);
-            let t = Point3::from(bc[i]);
-            window.draw_line(&s, &t, &red);
-        }
-
-        for i in 1..=100 {
-            let s = Point3::from(rbc[i - 1]);
-            let t = Point3::from(rbc[i]);
-            window.draw_line(&s, &t, &red);
-        }
         sleep(30);
     }
 }
--- src/geometry/bezier.rs
+++ src/geometry/bezier.rs
@@ -4,7 +4,7 @@ use na::allocator::Allocator;
 
 pub struct BezierCurve<D: Dim + DimName>
     where DefaultAllocator: Allocator<f32, D> {
-    pub control_points: Vec<VectorN<f32, D>>,
+    pub ctrlpts: Vec<VectorN<f32, D>>,
     pub degree: usize,
 }
 
@@ -17,14 +17,35 @@ pub struct LineSegment {
     pub end: Vector3<f32>
 }
 
+pub struct BSplineCurve<D: Dim + DimName>
+    where DefaultAllocator: Allocator<f32, D> {
+    pub ctrlpts: Vec<VectorN<f32, D>>,
+    pub knotvec: Vec<f32>,
+    pub degree: usize,
+}
+
+pub struct BSplineSurface<D: Dim + DimName>
+    where DefaultAllocator: Allocator<f32, D> {
+    pub control_points: Vec<VectorN<f32, D>>,
+    pub knot_vector_u: Vec<f32>,
+    pub knot_vector_v: Vec<f32>,
+    pub degree_u: usize,
+    pub degree_v: usize,
+    pub size_u: usize,
+    pub size_v: usize,
+}
+
+/// 3D Cartesian coordinates to Homogeneous coordinates
 pub fn cart2homo(vec: &Vector3<f32>) -> Vector4<f32> {
     return Vector4::new(vec[0], vec[1], vec[2], 1.0);
 }
 
+/// 3D Cartesian space vector to Homogeneous coordinates
 pub fn xvec2homo(vec: &Vector3<f32>) -> Vector4<f32> {
     return Vector4::new(vec[0], vec[1], vec[2], 0.0);
 }
 
+/// Homogeneous coordinates to 3D Cartesian coordinates
 pub fn homo2cart(vec: &Vector4<f32>) -> Option<Vector3<f32>> {
     if vec[3] == 0.0 {
         return None;
@@ -33,6 +54,7 @@ pub fn homo2cart(vec: &Vector4<f32>) -> Option<Vector3<f32>> {
     }
 }
 
+/// Homogeneous coordinates to 3D Cartesian space vector
 pub fn homo2xvec(vec: &Vector4<f32>) -> Option<Vector3<f32>> {
     if vec[3] == 0.0 {
         return Some(Vector3::new(vec[0], vec[1], vec[2]) / vec[3]);
@@ -73,7 +95,7 @@ impl<D: Dim + DimName> BezierCurve<D>
     pub fn new(control_points: Vec<VectorN<f32, D>>) -> Self {
         BezierCurve {
             degree: control_points.len() - 1,
-            control_points,
+            ctrlpts: control_points,
         }
     }
 
@@ -129,7 +151,7 @@ impl<D: Dim + DimName> BezierCurve<D>
         let bernstein = self.all_bernstein(u);
         let mut point = VectorN::zeros();
         for k in 0..=n {
-            point = point + bernstein[k] * &self.control_points[k];
+            point = point + bernstein[k] * &self.ctrlpts[k];
         }
         return point;
     }
@@ -137,7 +159,7 @@ impl<D: Dim + DimName> BezierCurve<D>
 
     /// De Casteljau Algorithm
     pub fn de_casteljau(&self, u: f32) -> VectorN<f32, D> {
-        let mut points = self.control_points.clone();
+        let mut points = self.ctrlpts.clone();
         let n = self.degree;
         for i in 1..=n {
             for k in 0..=n-i {
@@ -172,4 +194,124 @@ impl<D: Dim + DimName> BezierCurve<D>
     }
 }
 
+impl <D: Dim + DimName> BSplineCurve<D>
+    where DefaultAllocator: Allocator<f32, D> {
 
+    /// Create a B-Spline curve given knots
+    pub fn new(control_points: Vec<VectorN<f32, D>>, knot_vector: Vec<f32>) -> Self {
+        BSplineCurve {
+            degree: control_points.len() - 1,
+            ctrlpts: control_points,
+            knotvec: knot_vector,
+        }
+    }
+
+    /// Determine the knot span index
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - number of control points
+    /// * `p` - basis function degree
+    /// * `u` - parameter
+    /// * `knot_vec` - knot vector
+    pub fn find_span(n: usize, p: usize, u: f32, knot_vec: &Vec<f32>) -> usize {
+        if (u as f32 - knot_vec[n+1]).abs() < 1e-6 {
+            return n;
+        }
+
+        // Bisection search
+        let mut low: usize  = p;
+        let mut high: usize = n+1;
+        let mut mid: usize  =  (low + high) / 2;
+        while u < knot_vec[mid] || u >= knot_vec[mid+1] {
+            if u < knot_vec[mid] {
+                high = mid;
+            } else {
+                low = mid;
+            }
+            mid = (low + high) / 2;
+        }
+
+        return mid;
+    }
+
+
+    /// Compute all the non-vanishing basis functions (no division by zero)
+    ///
+    /// # Arguments
+    ///
+    /// * `i` - number of control points
+    /// * `u` - parameter
+    /// * `p` - basis function degree
+    /// * `knot_vec` - knot vector
+    pub fn basis_functions(i: usize, u: f32, p: usize, knot_vec: &Vec<f32>) -> Vec<f32> {
+        let mut basis: Vec<f32> = vec![0.; p+1];
+        let mut  left: Vec<f32> = vec![0.; p+1];
+        let mut right: Vec<f32> = vec![0.; p+1];
+
+        basis[0] = 1.0;
+        for j in 1..=p {
+            left[j]  = u - knot_vec[i+1-j];
+            right[j] = knot_vec[i+j] - u;
+            let mut saved = 0.;
+            for r in 0..j {
+                let temp = basis[r] / (right[r+1] + left[j-r]);
+                basis[r] = saved + right[r+1] * temp;
+                saved = left[j-r] * temp;
+            }
+            basis[j] = saved;
+        }
+
+        return basis;
+    }
+}
+
+impl <D: Dim + DimName> BSplineSurface<D>
+    where DefaultAllocator: Allocator<f32, D> {
+
+    pub fn new(control_points: Vec<VectorN<f32, D>>,
+               knot_vector_u: Vec<f32>,
+               knot_vector_v: Vec<f32>,
+               degree_u: usize,
+               degree_v: usize,
+               size_u: usize,
+               size_v: usize,) -> Self {
+        BSplineSurface {
+            degree_u,
+            degree_v,
+            control_points,
+            knot_vector_u,
+            knot_vector_v,
+            size_u,
+            size_v,
+        }
+    }
+
+    /// Evaluates the surface at the input (u, v) parameter pair.
+    /// Algorithm A3.5: SurfacePoint
+    ///
+    /// # Arguments
+    ///
+    /// * `u` - the first parameter
+    /// * `v` - the second parameter
+    pub fn evaluate_single(&self, u: f32, v: f32) -> VectorN<f32, D> {
+        let span_u = BSplineCurve::find_span(self.size_u, self.degree_u, u, &self.knot_vector_u);
+        let span_v = BSplineCurve::find_span(self.size_v, self.degree_v, v, &self.knot_vector_v);
+        let basis_u = BSplineCurve::basis_functions(span_u, u, self.degree_u, &self.knot_vector_u);
+        let basis_v = BSplineCurve::basis_functions(span_v, v, self.degree_v, &self.knot_vector_v);
+
+        let index_u = span_u - self.degree_u;
+        let mut point: VectorN<f32, D> = VectorN::zeros();
+        for j in 0..=self.degree_v {
+            let mut temp: VectorN<f32, D> = VectorN::zeros();
+            let index_v = span_v - self.degree_v + j;
+            for k in 0..=self.degree_u {
+                let index_ctrl_point = (index_u + k) * self.size_v + index_v;
+                temp = temp + basis_u[k] * &self.control_points[index_ctrl_point];
+            }
+            point = point + basis_v[j] * temp;
+        }
+
+        return point;
+    }
+}
